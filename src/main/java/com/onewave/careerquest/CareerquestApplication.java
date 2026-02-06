aea0f8baaa888aae1626af29daf8b1ad26244104
jjy12163 <lalalove0310@gmail.com>
2026-02-06 20:03:32 +0900

    init

---
+++ src/main/java/com/onewave/careerquest/CareerquestApplication.java
@@ -0,0 +1,13 @@
+package com.onewave.careerquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onewave/careerquest/CareerquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onewave.careerquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
